f2494253ce1c45225e36a40b61a77f44ace39521
jreus <hello@jonathanreus.com>
2019-10-22 11:17:24 +0100

    first commit

---
+++ BelaProjects/I2C_Input_Test/build/Trill.d
@@ -0,0 +1,19 @@
+/root/Bela/projects/I2C_Input_Test/build/Trill.o: \
+  /root/Bela/projects/I2C_Input_Test/Trill.cpp \
+  /root/Bela/projects/I2C_Input_Test/Trill.h include/I2c.h \
+  /usr/xenomai/include/cobalt/stdio.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/fcntl.h \
+  /usr/xenomai/include/cobalt/sys/ioctl.h
+
+/root/Bela/projects/I2C_Input_Test/Trill.h:
+
+include/I2c.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/fcntl.h:
+
+/usr/xenomai/include/cobalt/sys/ioctl.h:
---
+++ BelaProjects/I2C_Input_Test/build/render.d
@@ -0,0 +1,25 @@
+/root/Bela/projects/I2C_Input_Test/build/render.o: \
+  /root/Bela/projects/I2C_Input_Test/render.cpp include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  include/Utilities.h /root/Bela/projects/I2C_Input_Test/Trill.h \
+  include/I2c.h /usr/xenomai/include/cobalt/fcntl.h \
+  /usr/xenomai/include/cobalt/sys/ioctl.h
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+include/Utilities.h:
+
+/root/Bela/projects/I2C_Input_Test/Trill.h:
+
+include/I2c.h:
+
+/usr/xenomai/include/cobalt/fcntl.h:
+
+/usr/xenomai/include/cobalt/sys/ioctl.h:
